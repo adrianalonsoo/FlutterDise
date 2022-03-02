c19e9c8016343f9deb33b46c25afc80702ae61df
adrianalonsoo <adrianalonsoprados@gmail.com>
2022-03-02 23:47:50 +0100

    FlutterDise
    Flutter diseño

---
+++ main.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter_final/constants.dart';
+import 'package:flutter_final/controllers/menuController.dart';
+import 'package:flutter_final/screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Panel de Control',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuController(),
+          ),
+        ],
+        child: MainScreen(),
+      ),
+    );
+  }
+}
